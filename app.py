1597fe31f24bdf8b43ee9abcf1f363ae4767f52a
Michael B <mbashner@u.rochester.edu>
2020-11-18 19:07:04 -0500

    Add basic database READ function to app.py
    Add code to query petition table and return all petitions

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
+import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from models import *
 
 app = Flask(__name__)
 application = app
 bootstrap = Bootstrap(app)
 
+# SQLAlchemy and Database setup code
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+	return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	petitions = Petition.query.all()
+	return render_template('index.html', petitions=petitions)
