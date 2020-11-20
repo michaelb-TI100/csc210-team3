cd1f441f3fe0681ac34829dd78f15fc1a2523317
NateWebber <nwebber@u.rochester.edu>
2020-11-20 14:01:54 -0500

    better documentation in app.py
    added more commenting since I wasn't being good about it before

--- app.py
+++ app.py
@@ -20,16 +20,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-
+#TODO neither of these error handling pages seems to render quite right at the moment
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
 
-
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -40,6 +38,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = loginForm()
+	#particularly these two lines shouldn't be necessary once the app functions, but for now its to demonstrate the data is being handled correctly
 	email = None
 	password = None
 	if form.validate_on_submit():
@@ -48,6 +47,7 @@ def login():
 	#all this does is shows that the data was received correctly
 	return render_template('login.html', form=form, email=email, password=password)
 
+#at the moment register and login are practically the same
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	form = registerForm()
@@ -58,19 +58,24 @@ def register():
 		password = form.password.data
 	return render_template('register.html', form=form, email=email, password=password)
 
+
+#TODO eventually this will have to be modified so that you can't access this URL if you aren't currently logged in
 @app.route('/create', methods=['GET', 'POST'])
 def create():
 	form = petitionForm()
 	if form.validate_on_submit():
+		#grab data from the form
 		new_title = form.title.data
 		new_body = form.body.data
-		#for now, the 'test' user owns all new petitions
+		#for now, the 'test' user, whose id is 1, owns all new petitions
+		#in the future, we'll instead grab the author based on the current login session
 		new_author_id = 1
 		new_petition = Petition(title = new_title, body = new_body, author_id = new_author_id)
 		try:
 			db.session.add(new_petition)
 			db.session.commit()
 			return redirect(url_for('index'))
+		#this should never be able to happen
 		except:
 			return 'There was an error adding your new petition!'
 	return render_template('create.html', form=form)
