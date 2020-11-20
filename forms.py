9140a88476cec33bd065cbd6bd11fb768ba9a484
Michael B <mbashner@u.rochester.edu>
2020-11-20 14:55:56 -0500

    Update forms.py to use html5 emailField

--- forms.py
+++ forms.py
@@ -1,20 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
+from wtforms.fields.html5 import EmailField
 
 #document defines the various forms we will use 
 
 #I think it may be easier to just use emails instead of custom usernames. 
 class loginForm(FlaskForm):
-    email = StringField('Email Address', validators=[DataRequired()]) #no email validator yet, because I have to figure out csrf stuff
+    email = EmailField('Email Address', validators=[DataRequired()]) #no email validator yet, because I have to figure out csrf stuff
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 #TODO current plan is to make sure submitted emails are from rochester.edu domain. so, uh, do that
 class registerForm(FlaskForm):
-    email = StringField('Email Address', validators=[DataRequired()]) #still no email validator yet
+    email = EmailField('Email Address', validators=[DataRequired()]) #still no email validator yet
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 #form for submitting new petitions
 class petitionForm(FlaskForm):
